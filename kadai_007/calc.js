394755841d36a22a6f64c91789fb2a99441a86c4
nikomiko1972 <149290237+nikomiko1972@users.noreply.github.com>
2024-01-01 18:46:56 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+//変数の宣言
+let num;
+
+//値の代入
+num = 7;
+
+if (num%3 === 0 && num%5=== 0){
+  console.log('3と5の倍数です');
+}
+else if (num%3 === 0){
+  console.log('3の倍数です');
+}
+else if(num%5=== 0){
+  console.log('5の倍数です');
+}
+else {
+  console.log(num);
+}
